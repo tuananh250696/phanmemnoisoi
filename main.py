86fc853fbdd065cb9a3728f048bb06ed4c752da9
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2020-06-23 00:21:25 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ import random
 from tkinter import ttk
 from PIL import ImageTk,Image
 import cv2
+import sys
+import numpy as np
+from PyQt5 import QtCore
+from PyQt5.QtCore  import pyqtSlot
+from PyQt5.QtGui import QImage , QPixmap
+from PyQt5.QtWidgets import QDialog , QApplication
+from PyQt5.uic import loadUi
 
 
 conn = sqlite3.connect("Database\store.db")
@@ -187,16 +194,75 @@ class Application:
         self.bottom2.destroy()
 
     def endoscopy(self):
-        self.left.destroy()
-        self.left1.destroy()
+        #root.destroy()
 
-        vc = cv2.VideoCapture(0)
-        rval, frame = vc.read()
+        class tehseencode(QDialog):
+            def __init__(self):
+                super(tehseencode, self).__init__()
+                # loadUi("student3.ui",self)
+                loadUi("untitled2.ui", self)
 
-        self.right.destroy()
-        self.bottom.destroy()
-        self.bottom1.destroy()
-        self.bottom2.destroy()
+                self.logic = 0
+                self.value = 1
+                self.SHOW.clicked.connect(self.onClicked)
+                self.TEXT.setText("Kindly Press 'Show' to connect with webcam.")
+                self.CAPTURE.clicked.connect(self.CaptureClicked)
+                self.NEXT.clicked.connect(self.exitpro)
+            @pyqtSlot()
+
+
+
+            def onClicked(self):
+                self.TEXT.setText('Kindly Press "Capture Image " to Capture image')
+                cap = cv2.VideoCapture(0)
+                # while (True):
+                # print(cap.read())
+                while (cap.isOpened()):
+                    ret, frame = cap.read()
+
+                    if ret == True:
+                        print('here')
+                        self.displayImage(frame, 1)
+                        cv2.waitKey()
+                        if (self.logic == 2):
+                            self.value = self.value + 1
+                            cv2.imwrite('D:/projectpythonmanger/anh\%s.png' % (self.value), frame)
+                            self.logic = 1
+                            self.TEXT.setText('your Image have been Saved')
+                    else:
+                        print('not found')
+                cap.release()
+                cv2.destroyAllWindows()
+
+            def CaptureClicked(self):
+                self.logic = 2
+
+            def displayImage(self, img, window=1):
+                qformat = QImage.Format_Indexed8
+                if len(img.shape) == 3:
+                    if (img.shape[2]) == 4:
+                        qformat = QImage.Format_RGBA888
+                    else:
+                        qformat = QImage.Format_RGB888
+                img = QImage(img, img.shape[1], img.shape[0], qformat)
+                img = img.rgbSwapped()
+                self.imgLabel.setPixmap(QPixmap.fromImage(img))
+                self.imgLabel.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
+
+
+            def exitpro(self):
+                self.logic = 3
+
+        app = QApplication(sys.argv)
+        window = tehseencode()
+        window.show()
+        try:
+            sys.exit(app.exec_())
+        except:
+            print('excitng')
+
+
+
 
     def quit(self):
         root.destroy()
