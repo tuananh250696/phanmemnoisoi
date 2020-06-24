c51a1699754a202bb768c0accf410ea33423d982
tuananh250696 <35196486+tuananh250696@users.noreply.github.com>
2020-06-24 22:35:27 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,17 +6,16 @@ import math
 import os
 import random
 from tkinter import ttk
-from PIL import ImageTk,Image
+from PIL import ImageTk, Image
 import cv2
 import sys
 import numpy as np
 from PyQt5 import QtCore
-from PyQt5.QtCore  import pyqtSlot
-from PyQt5.QtGui import QImage , QPixmap
-from PyQt5.QtWidgets import QDialog , QApplication
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
-
-
+import imutils
 conn = sqlite3.connect("Database\store.db")
 c = conn.cursor()
 
@@ -37,7 +36,6 @@ image = PhotoImage(file="ngang1.png")
 img_resize = image.subsample(1, 1)
 
 
-
 class Application:
     def __init__(self, master, *args, **kwargs):
         self.master = master
@@ -50,29 +48,36 @@ class Application:
         self.left.pack(side=LEFT)
 
         # components
-        self.date_l = Label(self.left, text="Today's Date: " + str(date), font=('arial 16 bold'), bg='lightblue', fg='white')
+        self.date_l = Label(self.left, text="Today's Date: " + str(date), font=('arial 16 bold'), bg='lightblue',
+                            fg='white')
         self.date_l.place(x=20, y=0)
 
         # button
-        self.bt_st_catalog = Button(self.left, text="Hồ sơ bệnh nhân", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.ajax)
+        self.bt_st_catalog = Button(self.left, text="Hồ sơ bệnh nhân", width=18, height=2, font=('arial 18 bold'),
+                                    bg='orange', command=self.ajax)
         self.bt_st_catalog.place(x=8, y=45)
 
-        self.bt_st_form = Button(self.left, text="Nội soi", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.endoscopy)
+        self.bt_st_form = Button(self.left, text="Nội soi", width=18, height=2, font=('arial 18 bold'), bg='orange',
+                                 command=self.endoscopy)
         self.bt_st_form.place(x=8, y=135)
 
-        self.bt_patient = Button(self.left, text="Biểu mẫu in", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.ajax)
+        self.bt_patient = Button(self.left, text="Biểu mẫu in", width=18, height=2, font=('arial 18 bold'), bg='orange',
+                                 command=self.ajax)
         self.bt_patient.place(x=8, y=225)
 
-        self.bt_endoscop = Button(self.left, text="Danh mục in", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.ajax)
+        self.bt_endoscop = Button(self.left, text="Danh mục in", width=18, height=2, font=('arial 18 bold'),
+                                  bg='orange', command=self.ajax)
         self.bt_endoscop.place(x=8, y=315)
 
-        self.bt_diagnose = Button(self.left, text="Danh mục chẩn đoán", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.ajax)
+        self.bt_diagnose = Button(self.left, text="Danh mục chẩn đoán", width=18, height=2, font=('arial 18 bold'),
+                                  bg='orange', command=self.ajax)
         self.bt_diagnose.place(x=8, y=405)
 
-        self.bt_exit1 = Button(self.left, text="Thoát", width=18, height=2,  font=('arial 18 bold'),bg='orange', command=self.quit)
+        self.bt_exit1 = Button(self.left, text="Thoát", width=18, height=2, font=('arial 18 bold'), bg='orange',
+                               command=self.quit)
         self.bt_exit1.place(x=8, y=495)
 
-    def ajax(self,*args, **kwargs):
+    def ajax(self, *args, **kwargs):
         self.right = Frame(root, width=2100, height=53, bg='white')
         self.right.pack(side=TOP)
 
@@ -85,22 +90,27 @@ class Application:
         self.bottom2.pack(side=TOP)
 
         # button control system
-        self.bt_add_patient = Button(self.right, text="Thêm bệnh nhân", width=15, height=2, font=('arial 12 bold'), bg='white', command=self.ajax)
+        self.bt_add_patient = Button(self.right, text="Thêm bệnh nhân", width=15, height=2, font=('arial 12 bold'),
+                                     bg='white', command=self.ajax)
         self.bt_add_patient.place(x=0, y=0)
 
-        self.bt_open_file = Button(self.right, text="Mở hồ sơ", width=15, height=2, font=('arial 12 bold'),bg='white', command=self.ajax)
+        self.bt_open_file = Button(self.right, text="Mở hồ sơ", width=15, height=2, font=('arial 12 bold'), bg='white',
+                                   command=self.ajax)
         self.bt_open_file.place(x=160, y=0)
         #
-        self.bt_save_file = Button(self.right, text="lưu hồ sơ", width=15, height=2, font=('arial 12 bold'),bg='white', command=self.ajax)
+        self.bt_save_file = Button(self.right, text="lưu hồ sơ", width=15, height=2, font=('arial 12 bold'), bg='white',
+                                   command=self.ajax)
         self.bt_save_file.place(x=320, y=0)
         #
-        self.bt_delele1 = Button(self.right, text="Xóa", width=15, height=2, font=('arial 12 bold'), bg='white', command=self.ajax)
+        self.bt_delele1 = Button(self.right, text="Xóa", width=15, height=2, font=('arial 12 bold'), bg='white',
+                                 command=self.ajax)
         self.bt_delele1.place(x=480, y=0)
         #
-        self.bt_thoat = Button(self.right, text="Đóng", width=15, height=2, font=('arial 12 bold'), bg='white',command=self.add_to_cart)
+        self.bt_thoat = Button(self.right, text="Đóng", width=15, height=2, font=('arial 12 bold'), bg='white',
+                               command=self.add_to_cart)
         self.bt_thoat.place(x=640, y=0)
 
-        self.tenbenhnhan = Label(self.bottom, text="Tên bệnh nhân:", font=('arial 12 bold'), fg='black',bg='lightblue')
+        self.tenbenhnhan = Label(self.bottom, text="Tên bệnh nhân:", font=('arial 12 bold'), fg='black', bg='lightblue')
         self.tenbenhnhan.place(x=15, y=5)
 
         self.name_p = Entry(self.bottom, font=('arial 24 bold'), width=20)
@@ -140,10 +150,13 @@ class Application:
         #
         var3 = IntVar()
         var4 = IntVar()
-        self.chkbtn1 = Checkbutton(self.bottom, text='Nam', variable=var3, font=('arial 18 bold'), fg='black', bg='lightblue').place(x=810, y=25)
-        self.chkbtn2 = Checkbutton(self.bottom, text='Nữ', variable=var4, font=('arial 18 bold'), fg='black', bg='lightblue').place(x=900, y=25)
+        self.chkbtn1 = Checkbutton(self.bottom, text='Nam', variable=var3, font=('arial 18 bold'), fg='black',
+                                   bg='lightblue').place(x=810, y=25)
+        self.chkbtn2 = Checkbutton(self.bottom, text='Nữ', variable=var4, font=('arial 18 bold'), fg='black',
+                                   bg='lightblue').place(x=900, y=25)
 
-        self.seachinfo = Button(self.bottom1, text="Tìm kiếm", width=15, height=1, font=('arial 18 bold'), bg='orange',command=self.ajax)
+        self.seachinfo = Button(self.bottom1, text="Tìm kiếm", width=15, height=1, font=('arial 18 bold'), bg='orange',
+                                command=self.ajax)
         self.seachinfo.place(x=800, y=5)
 
         self.name_info = Label(self.bottom1, text="Tên:", font=('arial 12 bold'), fg='black', bg='lightblue')
@@ -153,10 +166,11 @@ class Application:
         self.name_infos.place(x=55, y=10)
 
         var1 = IntVar()
-        self.chkbtn3 =Checkbutton(self.bottom1, text="Nam", variable=var1, font=('arial 14 bold'), fg='black', bg='lightblue').place(x=260, y=10)
+        self.chkbtn3 = Checkbutton(self.bottom1, text="Nam", variable=var1, font=('arial 14 bold'), fg='black',
+                                   bg='lightblue').place(x=260, y=10)
         var2 = IntVar()
-        chkbtn4 =Checkbutton(self.bottom1, text="Nữ", variable=var2, font=('arial 14 bold'), fg='black', bg='lightblue').place(x=340, y=10)
-
+        chkbtn4 = Checkbutton(self.bottom1, text="Nữ", variable=var2, font=('arial 14 bold'), fg='black',
+                              bg='lightblue').place(x=340, y=10)
 
         self.date_s = Label(self.bottom1, text="Từ:", font=('arial 12 bold'), fg='black', bg='lightblue')
         self.date_s.place(x=420, y=15)
@@ -169,23 +183,25 @@ class Application:
         self.from_todate.place(x=620, y=10)
 
         # infseach
-        self.s_stt = Label(self.bottom2, text="Stt", font=('arial 12 bold'), fg='black', bg='lightblue',width=5)
+        self.s_stt = Label(self.bottom2, text="Stt", font=('arial 12 bold'), fg='black', bg='lightblue', width=5)
         self.s_stt.place(x=0, y=5)
-        self.s_name = Label(self.bottom2, text="Họ và tên", font=('arial 12 bold'), fg='black', bg='lightblue', width=20)
+        self.s_name = Label(self.bottom2, text="Họ và tên", font=('arial 12 bold'), fg='black', bg='lightblue',
+                            width=20)
         self.s_name.place(x=65, y=5)
-        self.s_address = Label(self.bottom2, text="Địa chỉ", font=('arial 12 bold'), fg='black', bg='lightblue',width=25)
+        self.s_address = Label(self.bottom2, text="Địa chỉ", font=('arial 12 bold'), fg='black', bg='lightblue',
+                               width=25)
         self.s_address.place(x=275, y=5)
-        self.s_sexgt = Label(self.bottom2, text="Giới tính", font=('arial 12 bold'), fg='black', bg='lightblue', width=8)
+        self.s_sexgt = Label(self.bottom2, text="Giới tính", font=('arial 12 bold'), fg='black', bg='lightblue',
+                             width=8)
         self.s_sexgt.place(x=535, y=5)
         self.s_born = Label(self.bottom2, text="Tuổi", font=('arial 12 bold'), fg='black', bg='lightblue', width=8)
         self.s_born.place(x=625, y=5)
-        self.s_job = Label(self.bottom2, text="Nghề nghiệp", font=('arial 12 bold'), fg='black', bg='lightblue', width=15)
+        self.s_job = Label(self.bottom2, text="Nghề nghiệp", font=('arial 12 bold'), fg='black', bg='lightblue',
+                           width=15)
         self.s_job.place(x=715, y=5)
-        self.s_sbh = Label(self.bottom2, text="Số bảo hiểm", font=('arial 12 bold'), fg='black', bg='lightblue',width=10)
+        self.s_sbh = Label(self.bottom2, text="Số bảo hiểm", font=('arial 12 bold'), fg='black', bg='lightblue',
+                           width=10)
         self.s_sbh.place(x=875, y=5)
-
-
-
 
     def add_to_cart(self, *args, **kwargs):
         self.right.destroy()
@@ -199,7 +215,6 @@ class Application:
         class tehseencode(QDialog):
             def __init__(self):
                 super(tehseencode, self).__init__()
-                # loadUi("student3.ui",self)
                 loadUi("untitled2.ui", self)
 
                 self.logic = 0
@@ -208,9 +223,8 @@ class Application:
                 self.TEXT.setText("Kindly Press 'Show' to connect with webcam.")
                 self.CAPTURE.clicked.connect(self.CaptureClicked)
                 self.NEXT.clicked.connect(self.exitpro)
+
             @pyqtSlot()
-
-
             def onClicked(self):
                 self.TEXT.setText('Kindly Press "Capture Image " to Capture image')
                 cap = cv2.VideoCapture(0)
@@ -218,6 +232,8 @@ class Application:
                 # print(cap.read())
                 while (cap.isOpened()):
                     ret, frame = cap.read()
+                    frame = imutils.resize(frame, width=1000)
+                    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
                     if ret == True:
                         print('here')
@@ -246,15 +262,11 @@ class Application:
                 img = QImage(img, img.shape[1], img.shape[0], qformat)
                 img = img.rgbSwapped()
                 self.imgLabel.setPixmap(QPixmap.fromImage(img))
-                self.imgLabel.setAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignVCenter)
-
+                self.imgLabel.setAlignment(QtCore.Qt.AlignLeft| QtCore.Qt.AlignLeft)
 
             def exitpro(self):
                 self.logic = 3
 
-
-
-        
         window = tehseencode()
         window.show()
         try:
@@ -262,11 +274,9 @@ class Application:
         except:
             print('excitng')
 
-
-
-
     def quit(self):
         root.destroy()
+
 
 app = QApplication(sys.argv)
 b = Application(root)
